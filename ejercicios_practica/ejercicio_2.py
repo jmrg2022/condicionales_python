e62eb466f0f128cb460a23d69ac8ae44c5214e4b
jmrg2022 <101527534+jmrg2022@users.noreply.github.com>
2022-03-24 21:46:44 -0300

    Add files via upload
    Ejercicios y Profundización 3er Clase

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -19,18 +19,39 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # Compare cual de las dos palabras es mayor (alfabéticamente)
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2:
+    print('{} es mayor que {}'.format(texto_1, texto_2))
+else:
+    print('{} es mayor que {}'.format(texto_2, texto_1))
+
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
 # Imprima en pantalla según corresponda
 
+if len(texto_1) > len(texto_2):
+   print(texto_1,'posee mayor cantidad de letras que',texto_2)
+else: 
+    print(texto_1,'posee menor cantidad de letras que',texto_2)
+
 # Verifique si la primera letra de la primera palabra
 # es mayor a la primera letra de la segunda palabra
 # Imprima en pantalla según corresponda
+
+if (texto_1[0]) > (texto_2[0]):
+   print('La primera letra de la primera palabra es MAYOR que la primera letra de la segurnda palabra')
+else: 
+    print('La primera letra de la primera palabra es MENOR que la primera letra de la segurnda palabra')
 
 copia_texto_1 = texto_1  # Copia de la variable texto_1
 
 # Verifique que copia_texto_1 es igual a texto_1
 # Imprima en pantalla según corresponda
 
+if (copia_texto_1 == texto_1):
+   print(copia_texto_1,'es igual a',texto_1)
+
 # Verifique que copia_texto_1 es distinta a texto_2
 # Imprima en pantalla según corresponda
+
+if (copia_texto_1 != texto_2):
+   print(copia_texto_1,'es distinto a',texto_2)
